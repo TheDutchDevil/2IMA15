8463946366db1639f8ff251b5dd133ea427e8440
Geert Verstegen <gverstegen@hotmail.com>
2016-12-30 15:09:43 +0100

    Added function to construct a bounding box.

---
+++ RandomizedIncremental.py
@@ -0,0 +1,49 @@
+from DataStructures import Vertex, Edge
+
+def constructBoundingBox(vertices, padding = 10):
+    """Constructs a bounding box around the provided list of vertices.
+    
+    Arguments:
+    vertices -- the list of vertices around which a bounding box is to be constructed.
+    padding -- the padding between the bounding box and outer vertices (default 10).
+    """
+    if vertices == []:
+        xLeft = 0
+        xRight = 0
+        yTop = 0
+        yBottom = 0
+    else:
+        xLeft = vertices[0].x
+        xRight = vertices[0].x
+        yTop = vertices[0].y
+        yBottom = vertices[0].y
+        
+        del vertices[0]
+        
+        for vertex in vertices:
+            if vertex.x < xLeft:
+                xLeft = vertex.x
+            if vertex.x > xRight:
+                xRight = vertex.x
+            
+            if vertex.y > yTop:
+                yTop = vertex.y
+            if vertex.y < yBottom:
+                yBottom = vertex.y
+    
+    topLeft = Vertex(xLeft - padding, yTop + padding)
+    topRight = Vertex(xRight + padding, yTop + padding)
+    bottomLeft = Vertex(xLeft - padding, yBottom - padding)
+    bottomRight = Vertex(xRight + padding, yBottom - padding)
+    
+    return BoundingBox(topLeft, topRight, bottomLeft, bottomRight)
+
+class BoundingBox:
+    def __init__(self, topLeft, topRight, bottomLeft, bottomRight):
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+        
+    def __repr__(self):
+        return "{}|\u203E \u203E|{} | {}|_ _|{}".format(self.topLeft, self.topRight, self.bottomLeft, self.bottomRight)
